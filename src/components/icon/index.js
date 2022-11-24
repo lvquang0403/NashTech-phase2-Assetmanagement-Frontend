9b6a0d8d0593ac2dab45b8199419320779b2f3f5
LEHOANGGLAM <lelam1384@gmail.com>
2022-11-25 05:02:02 +0700

    update UI change pass

--- src/components/icon/index.js
+++ src/components/icon/index.js
@@ -7,12 +7,9 @@ import {
 
 } from 'react-icons/fa';
 import { BsXCircle } from "react-icons/bs";
-import IconEyeOpen from "./IconEyeOpen";
-import IconEyeClose from "./IconEyeClose";
 
 export {
-    IconEyeOpen,
-    IconEyeClose,
+   
     FaFilter,
     FaSearch,
     FaCaretDown,
