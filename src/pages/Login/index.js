670bb54566becc86fcd222153c62b6abcb751960
Quang Vinh <vinhquang1873@gmail.com>
2022-11-29 03:54:30 +0000

    Merged PR 484: update message login error
    update message login error

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -53,7 +53,7 @@ const LoginPage = () => {
         })
         .catch(res => {
           Loading.remove();
-          toast.error('Username or Password Invalid', {
+          toast.error('Username or password is incorrect. Please try again', {
             position: toast.POSITION.TOP_CENTER,
             className: "border border-danger text-danger",
           });
